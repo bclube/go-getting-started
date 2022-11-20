76815ffa9e403a7f33474f100dadbb3608827d8b
Brian Lubeski <bclube@gmail.com>
2022-11-20 11:19:04 -0500

    Upgrade from mutex to read/write mutex for efficiency

--- concurrent-programming-with-go/main.go
+++ concurrent-programming-with-go/main.go
@@ -14,17 +14,17 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
 	wg := &sync.WaitGroup{}
-	m := &sync.Mutex{}
+	m := &sync.RWMutex{}
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
 		wg.Add(2)
-		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
 			defer wg.Done()
 			if b, ok := queryCache(id, m); ok {
 				fmt.Println("from cache\n", b)
 			}
 		}(id, wg, m)
-		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
 			defer wg.Done()
 			if b, ok := queryDatabase(id, m); ok {
 				fmt.Println("from database\n", b)
@@ -35,20 +35,20 @@ func main() {
 	wg.Wait()
 }
 
-func queryCache(id int, m *sync.Mutex) (models.Book, bool) {
-	m.Lock()
-	defer m.Unlock()
+func queryCache(id int, m *sync.RWMutex) (models.Book, bool) {
+	m.RLock()
 	b, ok := cache[id]
+	m.RUnlock()
 	return b, ok
 }
 
-func queryDatabase(id int, m *sync.Mutex) (models.Book, bool) {
+func queryDatabase(id int, m *sync.RWMutex) (models.Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range models.Books {
 		if b.ID == id {
 			m.Lock()
-			defer m.Unlock()
 			cache[id] = b
+			m.Unlock()
 			return b, true
 		}
 	}
