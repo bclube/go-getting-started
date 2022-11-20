26ce1e8b2d79eae918b0de3e7c7714526566dce1
Brian Lubeski <bclube@gmail.com>
2022-11-20 11:15:36 -0500

    Wait for all goroutines to finish with a wait group; fix race condition with mutex

--- concurrent-programming-with-go/main.go
+++ concurrent-programming-with-go/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 
 	"concurrent/models"
@@ -12,34 +13,41 @@ var cache = map[int]models.Book{}
 var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
+	wg := &sync.WaitGroup{}
+	m := &sync.Mutex{}
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
-		go func(id int) {
-			if b, ok := queryCache(id); ok {
-				fmt.Println("from cache")
-				fmt.Println(b)
+		wg.Add(2)
+		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
+			defer wg.Done()
+			if b, ok := queryCache(id, m); ok {
+				fmt.Println("from cache\n", b)
 			}
-		}(id)
-		go func(id int) {
-			if b, ok := queryDatabase(id); ok {
-				fmt.Println("from database")
-				fmt.Println(b)
+		}(id, wg, m)
+		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
+			defer wg.Done()
+			if b, ok := queryDatabase(id, m); ok {
+				fmt.Println("from database\n", b)
 			}
-		}(id)
+		}(id, wg, m)
 		time.Sleep(150 * time.Millisecond)
 	}
-	time.Sleep(2 * time.Second)
+	wg.Wait()
 }
 
-func queryCache(id int) (models.Book, bool) {
+func queryCache(id int, m *sync.Mutex) (models.Book, bool) {
+	m.Lock()
+	defer m.Unlock()
 	b, ok := cache[id]
 	return b, ok
 }
 
-func queryDatabase(id int) (models.Book, bool) {
+func queryDatabase(id int, m *sync.Mutex) (models.Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range models.Books {
 		if b.ID == id {
+			m.Lock()
+			defer m.Unlock()
 			cache[id] = b
 			return b, true
 		}
